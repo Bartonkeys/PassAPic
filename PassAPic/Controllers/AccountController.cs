49e76c8860508c54c93a53af4c937e3321fb7ff8
Graham McVea <graham.mcvea@gmail.com>
2014-05-23 08:36:32 +0100

    Add login explanation to Help page

--- PassAPic/Controllers/AccountController.cs
+++ PassAPic/Controllers/AccountController.cs
@@ -8,14 +8,11 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
-using System.Web.Http.ModelBinding;
-using System.Web.UI;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Linq;
 using Ninject;
 using PassAPic.Contracts;
 using PassAPic.Core.PushRegistration;
@@ -24,19 +21,47 @@ using PassAPic.Models;
 using PassAPic.Providers;
 using PassAPic.Results;
 
-
 namespace PassAPic.Controllers
 {
+    ////If user is registed it will return as follows, note the UserId, device needs to store this.
     /// <summary>
-    /// Responsible for registering users, logging in and out and letting client know who is online.
+    ///     Facebook Login:
+    ///     Responsible for registering users, logging in and out and letting client know who is online.
+    ///     GET http://passapic.apphb.com/api/Account/ExternalLogins?returnUrl=http://passapic.apphb.com&generateState=true
+    ///     which will return something like this:
+    ///     {
+    ///     Name: "Facebook"
+    ///     Url: "/api/Account/ExternalLogin?provider=Facebook&response_type=token&client_id=self&redirect_uri
+    ///     =http%3A%2F%2Fpassapic.apphb.com%2F&state=ZrU27TzJS0YPkdjWoB7Wh19FyKaeblBOkrkYZmTzTro1"
+    ///     State: "ZrU27TzJS0YPkdjWoB7Wh19FyKaeblBOkrkYZmTzTro1"
+    ///     }
+    ///     N.B State changes every time.
+    ///     Hit the Url, user logs in on facebook and an access token is returned.
+    ///     take that access token and hit UserInfo:
+    ///     GET: http://passapic.apphb.com/api/Account/UserInfo
+    ///     with this header:
+    ///     Authorization: Bearer
+    ///     <access token>
+    ///         {
+    ///         UserName: "Graham"
+    ///         HasRegistered: true
+    ///         LoginProvider: null
+    ///         UserId: 3
+    ///         }
+    ///     And you're done.
+    ///     If not hasRegistered returns false and device needs to call:
+    ///     POST: http://passapic.apphb.com/api/Account/RegisterExternal
+    ///     with this header: Authorization: Bearer <access token>
+    ///     and this in body: {"UserName":"Username"}
+    ///     and this returns 200 with userId
     /// </summary>
     [Authorize]
     [RoutePrefix("api/Account")]
     public class AccountController : BaseController
     {
         private const string LocalLoginProvider = "Local";
-        
-        
+
+
         [Inject]
         public AccountController(IUnitOfWork unitOfWork)
             : this(Startup.UserManagerFactory(), Startup.OAuthOptions.AccessTokenFormat)
@@ -56,9 +81,9 @@ namespace PassAPic.Controllers
 
         // POST api/Account/Anon
         /// <summary>
-        /// This is registration to get us started, so very simply. POST with JSON in the body with just username 
-        /// and API will return username and userId, which can be stored in device. 
-        /// Leave the username blank and API will generate a random 8 character username.
+        ///     This is registration to get us started, so very simply. POST with JSON in the body with just username
+        ///     and API will return username and userId, which can be stored in device.
+        ///     Leave the username blank and API will generate a random 8 character username.
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
@@ -70,7 +95,7 @@ namespace PassAPic.Controllers
             {
                 var newUser = new User
                 {
-                    Username = String.IsNullOrEmpty( model.Username) ? GenerateRandomUsername() : model.Username,
+                    Username = String.IsNullOrEmpty(model.Username) ? GenerateRandomUsername() : model.Username,
                 };
                 UnitOfWork.User.Insert(newUser);
                 UnitOfWork.Commit();
@@ -87,7 +112,7 @@ namespace PassAPic.Controllers
 
         // GET api/Account/Online
         /// <summary>
-        /// This API will return a list of online users.
+        ///     This API will return a list of online users.
         /// </summary>
         /// <returns></returns>
         [Route("Online")]
@@ -96,7 +121,7 @@ namespace PassAPic.Controllers
         {
             try
             {
-                var usersOnline =
+                List<AccountModel> usersOnline =
                     UnitOfWork.User.SearchFor(x => x.IsOnline)
                         .Select(y => new AccountModel {UserId = y.Id, Username = y.Username})
                         .ToList();
@@ -111,7 +136,7 @@ namespace PassAPic.Controllers
 
         // GET api/Account/AllUsers
         /// <summary>
-        /// This API will return a list of all registered users.
+        ///     This API will return a list of all registered users.
         /// </summary>
         /// <returns></returns>
         [Route("AllUsers")]
@@ -120,9 +145,9 @@ namespace PassAPic.Controllers
         {
             try
             {
-                var users =
+                List<AccountModel> users =
                     UnitOfWork.User.SearchFor(x => x.Id > 0)
-                        .Select(y => new AccountModel { UserId = y.Id, Username = y.Username })
+                        .Select(y => new AccountModel {UserId = y.Id, Username = y.Username})
                         .ToList();
                 return Request.CreateResponse(HttpStatusCode.OK, users);
             }
@@ -135,7 +160,7 @@ namespace PassAPic.Controllers
 
         // POST api/Account/Login
         /// <summary>
-        /// POST this baby up to let server know user is online.
+        ///     POST this baby up to let server know user is online.
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
@@ -145,7 +170,7 @@ namespace PassAPic.Controllers
         {
             try
             {
-                var user = UnitOfWork.User.GetById(userId);
+                User user = UnitOfWork.User.GetById(userId);
                 if (user == null) return Request.CreateResponse(HttpStatusCode.NotFound);
 
                 user.IsOnline = true;
@@ -153,8 +178,13 @@ namespace PassAPic.Controllers
                 UnitOfWork.User.Update(user);
                 UnitOfWork.Commit();
 
-                var results = user.Games.Select(y => new GamesModel{GameId = y.Id, StartingWord = y.StartingWord,
-                        NumberOfGuesses = y.NumberOfGuesses, GameOverMan = y.GameOverMan}).ToList();
+                List<GamesModel> results = user.Games.Select(y => new GamesModel
+                {
+                    GameId = y.Id,
+                    StartingWord = y.StartingWord,
+                    NumberOfGuesses = y.NumberOfGuesses,
+                    GameOverMan = y.GameOverMan
+                }).ToList();
 
                 return Request.CreateResponse(HttpStatusCode.OK, results);
             }
@@ -167,7 +197,7 @@ namespace PassAPic.Controllers
 
         // POST api/Account/Login
         /// <summary>
-        /// POST this bitch up to let server know user has fucked off.
+        ///     POST this bitch up to let server know user has fucked off.
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
@@ -177,7 +207,7 @@ namespace PassAPic.Controllers
         {
             try
             {
-                var user = UnitOfWork.User.GetById(userId);
+                User user = UnitOfWork.User.GetById(userId);
                 if (user == null) return Request.CreateResponse(HttpStatusCode.NotFound);
 
                 user.IsOnline = false;
@@ -196,20 +226,18 @@ namespace PassAPic.Controllers
 
         private String GenerateRandomUsername()
         {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             var random = new Random();
             return new string(
                 Enumerable.Repeat(chars, 8)
-                          .Select(s => s[random.Next(s.Length)])
-                          .ToArray());
-
+                    .Select(s => s[random.Next(s.Length)])
+                    .ToArray());
         }
-
 
 
         // POST api/Account/RegisterPush
         /// <summary>
-        /// POST a JObject up to register a device
+        ///     POST a JObject up to register a device
         /// </summary>
         /// <param name="jsonData"></param>
         /// <returns></returns>
@@ -218,7 +246,6 @@ namespace PassAPic.Controllers
         [HttpPost]
         public HttpResponseMessage RegisterPush([FromBody] RegisterItem registerItem)
         {
-           
             //If deviceType is not passed in - it will default to zero
             int deviceType = registerItem.DeviceType;
             int userId = registerItem.UserId;
@@ -230,7 +257,8 @@ namespace PassAPic.Controllers
 
             try
             {
-                var registration = UnitOfWork.PushRegister.SearchFor(x => x.DeviceToken == deviceToken).FirstOrDefault();
+                PushRegister registration =
+                    UnitOfWork.PushRegister.SearchFor(x => x.DeviceToken == deviceToken).FirstOrDefault();
                 if (registration != null)
                 {
                     if (registration.UserId == userId && deviceType == PushRegisterService.DeviceTypeIos)
@@ -243,21 +271,38 @@ namespace PassAPic.Controllers
                         //Droid and WP8 could have multiple device IDs linking user & device - its generated each time
                         //NB this code is identical to the base case below but we may vary for other devices
                         UnitOfWork.PushRegister.Delete(registration);
-                        UnitOfWork.PushRegister.Insert(new PushRegister() { UserId = userId, DeviceToken = deviceToken, DeviceType = deviceType, Timestamp = DateTime.Now });
+                        UnitOfWork.PushRegister.Insert(new PushRegister
+                        {
+                            UserId = userId,
+                            DeviceToken = deviceToken,
+                            DeviceType = deviceType,
+                            Timestamp = DateTime.Now
+                        });
                     }
 
                     else
                     {
                         //deviceToken associated with different member ID so remove this record to prevent duplicate push meassages
                         UnitOfWork.PushRegister.Delete(registration);
-                        UnitOfWork.PushRegister.Insert(new PushRegister() { UserId = userId, DeviceToken = deviceToken, DeviceType = deviceType, Timestamp = DateTime.Now });
+                        UnitOfWork.PushRegister.Insert(new PushRegister
+                        {
+                            UserId = userId,
+                            DeviceToken = deviceToken,
+                            DeviceType = deviceType,
+                            Timestamp = DateTime.Now
+                        });
                     }
-                    
                 }
                 else
                 {
                     //No entry for this deviceToken - so create a new one
-                    UnitOfWork.PushRegister.Insert(new PushRegister() { UserId = userId, DeviceToken = deviceToken, DeviceType = deviceType, Timestamp = DateTime.Now });
+                    UnitOfWork.PushRegister.Insert(new PushRegister
+                    {
+                        UserId = userId,
+                        DeviceToken = deviceToken,
+                        DeviceType = deviceType,
+                        Timestamp = DateTime.Now
+                    });
                 }
 
                 UnitOfWork.Commit();
@@ -270,37 +315,30 @@ namespace PassAPic.Controllers
                 {
                     return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
                 }
-                else
-                {throw;}
+                throw;
             }
 
 
             return Request.CreateResponse(HttpStatusCode.Created, new ReturnMessage(msg));
-       
         }
 
-       
-
         #region Lets Comment out this code for a while
-
-
-
 
         // GET api/Account/UserInfo
         [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
         [Route("UserInfo")]
         public UserInfoViewModel GetUserInfo()
         {
-            var externalLogin = ExternalLoginData.FromIdentity(User.Identity as ClaimsIdentity);
-            var aspNetUserId = User.Identity.GetUserId();
-            
+            ExternalLoginData externalLogin = ExternalLoginData.FromIdentity(User.Identity as ClaimsIdentity);
+            string aspNetUserId = User.Identity.GetUserId();
+
             return new UserInfoViewModel
             {
                 UserName = User.Identity.GetUserName(),
                 HasRegistered = externalLogin == null,
                 LoginProvider = externalLogin != null ? externalLogin.LoginProvider : null,
                 UserId = UnitOfWork.User
-                .SearchFor(x => x.AspNetUserId == aspNetUserId).Select(y => y.Id).FirstOrDefault()
+                    .SearchFor(x => x.AspNetUserId == aspNetUserId).Select(y => y.Id).FirstOrDefault()
             };
         }
 
@@ -512,7 +550,7 @@ namespace PassAPic.Controllers
             else
             {
                 IEnumerable<Claim> claims = externalLogin.GetClaims();
-                ClaimsIdentity identity = new ClaimsIdentity(claims, OAuthDefaults.AuthenticationType);
+                var identity = new ClaimsIdentity(claims, OAuthDefaults.AuthenticationType);
                 Authentication.SignIn(identity);
             }
 
@@ -525,7 +563,7 @@ namespace PassAPic.Controllers
         public IEnumerable<ExternalLoginViewModel> GetExternalLogins(string returnUrl, bool generateState = false)
         {
             IEnumerable<AuthenticationDescription> descriptions = Authentication.GetExternalAuthenticationTypes();
-            List<ExternalLoginViewModel> logins = new List<ExternalLoginViewModel>();
+            var logins = new List<ExternalLoginViewModel>();
 
             string state;
 
@@ -541,7 +579,7 @@ namespace PassAPic.Controllers
 
             foreach (AuthenticationDescription description in descriptions)
             {
-                ExternalLoginViewModel login = new ExternalLoginViewModel
+                var login = new ExternalLoginViewModel
                 {
                     Name = description.Caption,
                     Url = Url.Route("ExternalLogin", new
@@ -550,7 +588,7 @@ namespace PassAPic.Controllers
                         response_type = "token",
                         client_id = Startup.PublicClientId,
                         redirect_uri = new Uri(Request.RequestUri, returnUrl).AbsoluteUri,
-                        state = state
+                        state
                     }),
                     State = state
                 };
@@ -570,7 +608,7 @@ namespace PassAPic.Controllers
                 return BadRequest(ModelState);
             }
 
-            IdentityUser user = new IdentityUser
+            var user = new IdentityUser
             {
                 UserName = model.UserName
             };
@@ -604,7 +642,7 @@ namespace PassAPic.Controllers
                 return InternalServerError();
             }
 
-            IdentityUser user = new IdentityUser
+            var user = new IdentityUser
             {
                 UserName = model.UserName
             };
@@ -642,6 +680,7 @@ namespace PassAPic.Controllers
 
             base.Dispose(disposing);
         }
+
         #endregion
 
         #region Helpers
@@ -730,20 +769,20 @@ namespace PassAPic.Controllers
 
         private static class RandomOAuthStateGenerator
         {
-            private static RandomNumberGenerator _random = new RNGCryptoServiceProvider();
+            private static readonly RandomNumberGenerator _random = new RNGCryptoServiceProvider();
 
             public static string Generate(int strengthInBits)
             {
                 const int bitsPerByte = 8;
 
-                if (strengthInBits % bitsPerByte != 0)
+                if (strengthInBits%bitsPerByte != 0)
                 {
                     throw new ArgumentException("strengthInBits must be evenly divisible by 8.", "strengthInBits");
                 }
 
-                int strengthInBytes = strengthInBits / bitsPerByte;
+                int strengthInBytes = strengthInBits/bitsPerByte;
 
-                byte[] data = new byte[strengthInBytes];
+                var data = new byte[strengthInBytes];
                 _random.GetBytes(data);
                 return HttpServerUtility.UrlTokenEncode(data);
             }
