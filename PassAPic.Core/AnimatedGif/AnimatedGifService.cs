8e2352b27c301940b25ddc2a86781c25d1f4b386
Graham McVea <graham.mcvea@gmail.com>
2014-06-08 17:49:25 +0100

    Bit of an aul clear out, hopefully wont break anything

--- PassAPic.Core/AnimatedGif/AnimatedGifService.cs
+++ PassAPic.Core/AnimatedGif/AnimatedGifService.cs
@@ -16,13 +16,6 @@ namespace PassAPic.Core.AnimatedGif
 {
     public class AnimatedGifService
     {
-        
-		/* create Gif */
-		//you should replace filepath
-        //private static String filePathLocal = "C:\\YerMA\\Pass-a-pic\\Server\\Gif\\Example\\Res\\";
-        //private static String [] imageFilePaths = new String[]{filePathLocal+"01.png",filePathLocal+"02.png",filePathLocal+"03.png"}; 
-		//private static readonly String outputFilePath = filePathLocal+"test.gif";
-        private static readonly AnimatedGifEncoder _animatedGifEncoder = new AnimatedGifEncoder();
         private readonly CloudImageService _cloudImageService;
         private readonly IUnitOfWork _unitOfWork;
 
@@ -30,53 +23,7 @@ namespace PassAPic.Core.AnimatedGif
         {
             _cloudImageService = cloudImageService;
             _unitOfWork = unitOfWork;
-        }
-       
-
-        public static Image CreateAnimatedGifFromBitmapArray(String [] imageFilePaths, Boolean setRepeat, String outputFilePath) 
-        {
-            try
-            {
-                _animatedGifEncoder.Start(outputFilePath);
-                _animatedGifEncoder.SetDelay(1000);
-                //-1:no repeat,0:always repeat
-                _animatedGifEncoder.SetRepeat(setRepeat ? 0 : -1);
-                for (int i = 0, count = imageFilePaths.Length; i < count; i++)
-                {
-                    _animatedGifEncoder.AddFrame(Image.FromFile(imageFilePaths[i]));
-                }
-                _animatedGifEncoder.Finish();
-                return new Bitmap(outputFilePath);
-            }
-
-            catch (Exception e)
-            {
-                return null;
-            }
-            
-
-            
-        }
-		
-        public static Image[] ExtractBitmapsFromAnimatedGif(String pathToAnimatedGif)
-        {
-            Image[] frames;
-
-            /* extract Gif */
-            //string outputPath = "c:\\";
-            GifDecoder gifDecoder = new GifDecoder();
-            gifDecoder.Read(pathToAnimatedGif);
-            frames = new Image[gifDecoder.GetFrameCount()];
-            for (int i = 0, count = frames.Length; i < count; i++)
-            {
-                Image frame = gifDecoder.GetFrame(i);  // frame i
-                frames[i] = frame;
-                //frame.Save(outputPath + Guid.NewGuid().ToString() + ".png", ImageFormat.Png);
-            }
-
-            return frames;
-        }
-
+        }     
 
         public void CreateAnimatedGif(int gameId, string tempAnimatedGif)
         {
@@ -84,11 +31,11 @@ namespace PassAPic.Core.AnimatedGif
             {
                 var game = _unitOfWork.Game.SearchFor(x => x.Id == gameId).FirstOrDefault();
 
-                using (var collection = new MagickImageCollection())
+                using (var magickImageCollection = new MagickImageCollection())
                 {
                     var startingWordImage = TextToImageConversion.CreateBitmapImage("Start: " + game.StartingWord);
                     var startingMagickImage = new MagickImage(startingWordImage) {AnimationDelay = 300};
-                    collection.Add(startingMagickImage);
+                    magickImageCollection.Add(startingMagickImage);
 
                     var count = 1;
                     foreach (var guess in game.Guesses)
@@ -107,7 +54,7 @@ namespace PassAPic.Core.AnimatedGif
                             {
                                 AnimationDelay = 300
                             };
-                            collection.Add(magickWordImage);
+                            magickImageCollection.Add(magickWordImage);
                         }
                         else if (guess is ImageGuess)
                         {
@@ -122,17 +69,17 @@ namespace PassAPic.Core.AnimatedGif
                             {
                                 AnimationDelay = 300
                             };
-                            collection.Add(magickImage);
+                            magickImageCollection.Add(magickImage);
                         }
                         count++;
                     }
 
                     var settings = new QuantizeSettings {Colors = 256};
-                    collection.Quantize(settings);
+                    magickImageCollection.Quantize(settings);
 
-                    collection.Optimize();
+                    magickImageCollection.Optimize();
 
-                    collection.Write(tempAnimatedGif);
+                    magickImageCollection.Write(tempAnimatedGif);
                 }
 
                 game.AnimatedResult  = _cloudImageService.SaveImageToCloud(tempAnimatedGif);
