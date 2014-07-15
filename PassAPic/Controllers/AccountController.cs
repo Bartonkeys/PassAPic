50e114dc2920f54139d769fa5b75baafd0c4d38c
Michael Barr <michaelcbarr@hotmail.com>
2014-07-15 22:28:14 +0100

    - cleanup Gs dirty laundry.....again....

--- PassAPic/Controllers/AccountController.cs
+++ PassAPic/Controllers/AccountController.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Security;
-using Microsoft.Owin.Security;
 using Ninject;
 using PassAPic.Contracts;
 using PassAPic.Core.AccountManagement;
