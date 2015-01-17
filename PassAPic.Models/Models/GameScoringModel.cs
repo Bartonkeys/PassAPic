60134e83424df4008ee54b092925c8dfc9f71f88
Michael Barr <michaelcbarr@hotmail.com>
2015-01-17 23:43:00 +0000

    - add json properties t  Models/GameScoringModel.cs

--- PassAPic.Models/Models/GameScoringModel.cs
+++ PassAPic.Models/Models/GameScoringModel.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PassAPic.Models.Models
 {
     public class GameScoringModel
     {
+         
+        [JsonProperty(PropertyName = "gameId")]
         public int GameId { get; set; }
+        [JsonProperty(PropertyName = "userId")]
         public int UserId { get; set; }
+        [JsonProperty(PropertyName = "userName")]
         public string UserName { get; set; }
+        [JsonProperty(PropertyName = "score")]
         public int Score { get; set; }
 
     }
